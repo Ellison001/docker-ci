6e902a49c95fbbc77551129f2bf5111c4a5e0eed
Ellison Zhang <ezhang@wl00070043.active.local>
2018-07-05 23:45:04 +0800

    update app

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var request = require("supertest"),
-    app = require("../server").getApp;
+    app = require("../app").getApp;
 
 describe('GET /', function(){
   it('expects HTTP response 200', function(done){
