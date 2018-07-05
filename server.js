f9ae7e24271bdb0e454186b12caf36f0a15e78b2
Ellison Zhang <ezhang@Ellisons-MacBook-Pro.local>
2018-07-05 17:03:31 +0800

    update port

--- server.js
+++ server.js
@@ -2,13 +2,15 @@
 
 const express = require('express');
 
-const PORT = '8080';
+// Constants
+const PORT = 9091;
 const HOST = '0.0.0.0';
 
+// App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Docker\n');
+  res.send('Hello world\n');
 });
 
-app.listen(PORT, HOST)
-console.log(`Running on http://${HOST}:{PORT}`);
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
