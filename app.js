5a8d567dc98de815e042d6770d1ed45fbd7ebf3d
Ellison Zhang <ellison.zhang@aspiraconnect.com>
2019-07-14 22:12:34 +0800

    break the app again

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 201;
+const server = httpp.createServer((req, res) => {
+  res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World!\n');
 });
